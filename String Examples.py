00f02b4f3fab0b3ec62a53b636592c6dacfe67a6
Arifian Saputra <46412309+arifian853@users.noreply.github.com>
2021-01-25 22:03:50 +0700

    Add files via upload

---
+++ String Examples.py
@@ -0,0 +1,49 @@
+a = "Hello"
+print(a)
+
+print(" - Print variable a")
+
+print("-----------------------------------")
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+print(" - Multiline String")
+
+print("-----------------------------------")
+
+a = "Hello, World!"
+print(a[3])
+
+print(" - Array index 3 from word 'Hello World' is l")
+
+print("-----------------------------------")
+
+for x in "banana":
+  print(x)
+
+print(" - String in For loop")
+
+print("-----------------------------------")
+
+a = "Hello, World!"
+print(len(a))
+
+print(" - Print length of the word")
+
+print("-----------------------------------")
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("Yes, 'expensive' is NOT present.")
+
+print(" - String Check")
+
+print("-----------------------------------")
