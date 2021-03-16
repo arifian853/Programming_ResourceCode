eb5e6f78dd474a105735ba2cffd8e2090bc1ae9b
Arifian Saputra <arifiansaputra43@gmail.com>
2021-03-16 21:11:57 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      String name = "Halo";
+      String name2 = " Kamu !";
+      System.out.println(name+name2);
+      String name3 = "John";
+      System.out.println("Hello " + name3);
+      //Example of string in Java.
+    }
+  }
